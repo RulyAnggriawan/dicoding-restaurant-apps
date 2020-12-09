d453a45b66daf98d221348b07b771dab05268569
Ruly Anggriawan <ruly.anggriawan@live.com>
2020-12-09 11:05:03 +0700

    before off canvas menu

--- src/scripts/restaurant-item.js
+++ src/scripts/restaurant-item.js
@@ -7,6 +7,50 @@ class RestaurantItem extends HTMLElement{
     
     render(){
         this.innerHTML = `
+        <style>
+            .restaurant{
+                position: relative;
+                padding-bottom: 50px;
+            }
+            
+            .cover{
+                height: 50%;
+            }
+            
+            .cover img{
+                width: 100%;
+                height: 350px;
+                object-fit: cover;
+            }
+            
+            .content{
+                margin: 15px 15px 15px 15px;
+            }
+            
+            .city{
+                position: absolute;
+                top : 20px;
+                left: 20px;
+                font-weight: bold;
+                font-size: large;
+                color: black;
+                padding: 10px;
+                background-color: rgba(255, 255, 255, 0.5);
+                border-radius: 5px;
+                min-width: 100px;
+                text-align: center;
+            }
+            
+            .name{
+                font-weight: bold;
+                font-size: 1.5em;
+                margin: 5px 0;
+            }
+            
+            .description{
+                text-align: justify;
+            }
+        </style>
         <article class="restaurant">
             <div class="cover">
                 <p class="city">${this.city}</p>
